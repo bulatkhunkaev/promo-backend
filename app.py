9949a52cfb3564da042f004af14ba7867ad2b15a
Bulat Khunkaev <bulatkhunkaev@gmail.com>
2025-05-31 00:54:30 +0300

    fix: bind Flask to 0.0.0.0 for Render

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from flask_jwt_extended import JWTManager
 from config import Config
 from db import db
 from routes import routes
+import os
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -20,4 +21,5 @@ app.register_blueprint(routes)
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
